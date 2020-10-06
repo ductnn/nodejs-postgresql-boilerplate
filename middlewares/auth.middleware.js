e9da05650b7ba8aa85e9c0caa8df0ff156df8fc2
DuctnNetwork <trannhuduc531998@gmail.com>
2020-10-06 16:52:28 +0700

    Fix comment

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -8,14 +8,8 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 };
 
 module.exports.checkToken = (req, res, next) => {
-    // if(!req.session.userId){
-    //     return res.redirect('/auth/login');
-    // };
-    // const token = req.headers.authorization;
-
     try {
         const token = localStorage.getItem('token');
-        // const token = req.body.token || req.query.token || req.headers["x-access-token"];
             jwt.verify(token, secretKey, (err, payload) => {
                 if (payload) {
                     req.user = payload;
