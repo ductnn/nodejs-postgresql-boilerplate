a59f8800b1e09faaba939380a57548ee914fb60c
DuctnNetwork <trannhuduc531998@gmail.com>
2020-10-06 16:52:36 +0700

    Fix comment

--- models/user.model.js
+++ models/user.model.js
@@ -204,20 +204,3 @@ function hashPassword(password) {
     });
   });
 }
-
-
-
-
-
-
-
-
-
-
-// const Pool = require('pg').Pool;
-
-// const userPool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
-
-// module.exports = userPool;
